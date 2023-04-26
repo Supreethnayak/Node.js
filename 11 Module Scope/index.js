744582f25b471c0b2614e0088a60e47aa33bdcfa
Supreeth Nayak <59947774+Supreethnayak@users.noreply.github.com>
2023-04-26 18:01:53 +0530

    module scope added

---
+++ 11 Module Scope/index.js
@@ -0,0 +1,5 @@
+require("./batman");
+require("./superman");
+// each module has its own scope - IIFE
+// every module is wrapped in iffe before running it
+// function and variable name can be repeated
